0b6f82724cf5837c8dc6d7bed6733359ed93c5ca
Mohd Anas <mohdanas08022006@gmail.com>
2026-04-15 12:44:11 +0530

    some functions added

--- Graph/first.cpp
+++ Graph/first.cpp
@@ -1,7 +1,75 @@
 #include<iostream>
 #include<fstream>
 using namespace std;
+template<typename T>
+class Queue{
 
+   T *arr;
+   int front;
+   int rear;
+   int size;
+   int capacity;
+    public:
+
+   Queue(int n){
+      arr=new T[n];
+      capacity=n;
+      front=0;
+      rear=-1;
+
+      size=0;
+   }
+
+
+   bool isFull(){
+
+    return size==capacity;
+
+   }
+   bool isempty(){
+
+    return size==0;
+
+   }
+   void push(T x){
+       if(isFull()){
+        cout<<"queue is full";
+        return ;
+
+       }
+       rear=(rear+1)%capacity;
+       arr[rear]=x;
+       size++;
+       
+   }
+   void pop(){
+    if(isempty()){
+        cout<<"queue is empty";
+        return ;
+
+    }
+
+    front=(front+1)%capacity;
+    size--;
+
+   }
+   T Front(){
+        if(isempty()){
+            cout<<"queue is empty";
+            return T();
+
+
+        }
+
+        return arr[front];
+
+   }
+
+   int getsize(){
+    return size;
+
+   }
+};
 
 class graph{
     int v;
@@ -29,7 +97,6 @@ class graph{
         if(u>=this->v || v>=this->v )return ;
 
         mat[u][v]=1;
-        mat[v][u]=1;
 
     }
 
@@ -86,37 +153,57 @@ class graph{
         }
     }
 
+    void topologicalSort(){
+        vector<int>indeg(v,0);
+
+        for(int i=0;i<v;i++){
+            for(int j=0;j<v;j++){
+               if(mat[i][j]==1){
+                indeg[j]++;
+                
+               }
+
+            }
+        }
+        queue<int>q;
+        for(int i=0;i<v;i++){
+            if(indeg[i]==0){
+                q.push(i);
+
+            }
+        }
+
+
+        while(!q.empty()){
+            int x=q.front();
+            q.pop();
+            cout<<x<<" ";
+
+            for(int i=0;i<v;i++){
+                if(mat[x][i]==1){
+                    indeg[i]--;
+                    if(indeg[i]==0)q.push(i);
+
+                }
+            }
+        }
+
+    }
+
 };
 
 int main(){
 
-    graph g(7);
-    ifstream file("input.txt");
+     ifstream file("input.txt");
+     int v=6;
+     graph g(v);
 
-    int x,y;
-    while(file>>x>>y){
-
-
+     int x,y;
+     while(file>>x>>y){
         g.addEdge(x,y);
 
-
-    }
-
-    vector<bool>visited(7,0);
-
-    for(int i=0;i<7;i++){
-        if(!visited[i]){
-            g.bfs(i,visited);
-            cout<<" | ";
-
-
-        }
-    }
-
-
-
-
-
+     }
+     g.topologicalSort();
     return 0;
 
 }
